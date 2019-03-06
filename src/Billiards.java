64648f731e0564506ffb900378e44cef2040d2a4
Eduardo Zambrano León <eduardo.z.l662@gmail.com>
2019-03-06 09:41:11 +0100

    Add files via upload

---
+++ src/Billiards.java
@@ -0,0 +1,113 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+@SuppressWarnings("serial")
+public class Billiards extends JFrame {
+
+	public static int Width = 800;
+	public static int Height = 600;
+
+	private JButton b_start, b_stop;
+
+	private Board board;
+
+	// TODO update with number of group label. See practice statement.
+	private final int N_BALL = 5;
+	private Ball[] balls;
+	private Hilo[] hilos;
+	
+	private class Hilo extends Thread {
+		private Ball ball;
+		
+		public Hilo(Ball ball) {
+			this.ball = ball;
+		}
+		
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					ball.move();
+					board.repaint();
+					Thread.sleep(10);
+					
+				}
+				catch (InterruptedException e) {
+					return;
+				}
+			}
+		}
+	}
+
+	public Billiards() {
+
+		board = new Board();
+		board.setForeground(new Color(0, 128, 0));
+		board.setBackground(new Color(0, 128, 0));
+
+		initBalls();
+
+		b_start = new JButton("Empezar");
+		b_start.addActionListener(new StartListener());
+		b_stop = new JButton("Parar");
+		b_stop.addActionListener(new StopListener());
+
+		JPanel p_Botton = new JPanel();
+		p_Botton.setLayout(new FlowLayout());
+		p_Botton.add(b_start);
+		p_Botton.add(b_stop);
+
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(board, BorderLayout.CENTER);
+		getContentPane().add(p_Botton, BorderLayout.PAGE_END);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(Width, Height);
+		setLocationRelativeTo(null);
+		setTitle("Práctica programación concurrente objetos móviles independientes");
+		setResizable(false);
+		setVisible(true);
+	}
+
+	private void initBalls() {
+		// TODO init balls
+		balls = new Ball[N_BALL];
+		for (int i = 0; i < N_BALL; i++) {
+			balls[i] = new Ball();
+		}
+		board.setBalls(balls);
+	}
+
+	private class StartListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Code is executed when start button is pushed
+			hilos = new Hilo[N_BALL];
+			for (int i = 0; i < N_BALL; i++) {
+				hilos[i] = new Hilo(balls[i]);
+				hilos[i].start();
+			}
+		}
+	}
+
+	private class StopListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Code is executed when stop button is pushed
+			for (int i = 0; i < N_BALL; i++) {
+				hilos[i].interrupt();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new Billiards();
+	}
+}
